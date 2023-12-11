08465cb74ee79f8a7a8fc2cfd2245620766ed637
Andrea Lopez <andrealoga33@gmail.com>
2023-12-11 13:04:31 +0100

    Create footer component and given to it clodinary img  and some style

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,5 @@
 // app.component.jsx
+import { Footer } from '../footer/footer';
 import { Header } from '../header/header';
 import { AppRoutes } from '../routes/app.routes';
 
@@ -6,7 +7,7 @@ export function App() {
   return (
     <>
       <Header></Header>
-      <AppRoutes></AppRoutes>;
+      <AppRoutes></AppRoutes>;<Footer></Footer>
     </>
   );
 }
--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,1 +1,19 @@
+import './footer.scss';
 
+export function Footer() {
+  const cloudinaryImageUrl =
+    'https://res.cloudinary.com/dgnncaecc/image/upload/v1702295679/Footer_Logo_l9epde.png';
+  return (
+    <footer>
+      <div className="logo-footer" />
+      <img
+        src={cloudinaryImageUrl}
+        alt="Footer Logo"
+        width="100"
+        height="100"
+      />
+      <div className="logo-text" />
+      <p>逆説</p>
+    </footer>
+  );
+}
