9692e6d1a6ef551976cbf1bb33e740c9dee3302d
Andrea Lopez <andrealoga33@gmail.com>
2023-12-12 13:47:23 +0100

    Fill up films repo with the rest of its methods

--- src/services/api.repo.films.ts
+++ src/services/api.repo.films.ts
@@ -3,9 +3,48 @@ import { Film } from '../entities/film';
 
 export class FilmsRepo {
   apiUrl = serverUrl + '/films';
+  constructor(public token: string) {}
 
-  async getAll(): Promise<Film[]> {
+  async getFilms(): Promise<Film[]> {
     const response = await fetch(this.apiUrl);
+    if (!response.ok)
+      throw new Error(response.status + ' ' + response.statusText);
+    return response.json();
+  }
+  // Async getFilmById(id:string, updatedItem:Partial<Film>) Promise<Film>{
+  //   const url = this.apiUrl
+  //   const response = await fetch(this.apiUrl)
+  // }
+
+  async createFilm(newFilm: FormData): Promise<Film> {
+    const response = await fetch(this.apiUrl, {
+      method: 'POST',
+      body: JSON.stringify(newFilm),
+      headers: { Authorization: 'Bearer ' + this.token },
+    });
+    if (!response.ok)
+      throw new Error(response.status + ' ' + response.statusText);
+    return response.json();
+  }
+
+  async updateFilm(id: Film['id'], updatedItem: Partial<Film>): Promise<Film> {
+    const finalUrl = `${this.apiUrl}/${id}`;
+    const response = await fetch(this.finalUrl, {
+      method: 'POST',
+      body: JSON.stringify(updatedItem),
+      headers: { Authorization: 'Bearer' },
+    });
+    if (!response.ok)
+      throw new Error(response.status + ' ' + response.statusText);
+    return response.json();
+  }
+
+  deleteFilm(id: Film['id']): Promise<Film[]> {
+    const finalUrl = `${this.apiUrl}/${id}`;
+    const response = await fetch(this.finalUrl, {
+      method: 'POST',
+      headers: { Authorization: 'Bearer' },
+    });
     if (!response.ok)
       throw new Error(response.status + ' ' + response.statusText);
     return response.json();
